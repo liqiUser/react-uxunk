36e80c710a12afc51c9706bafedf0bdaffa13f59
qi.li <787139384@qq.com>
2018-08-27 23:57:25 +0800

    docs: react-redux v15 注释添加

--- src/library/react-redux/react-redux_v15/connect.jsx
+++ src/library/react-redux/react-redux_v15/connect.jsx
@@ -5,13 +5,23 @@ import PropTypes from 'prop-types';
 const connect = (mapState, mapDispatch) => (WrappedComponent) => {
 
     class Connect extends React.Component {
+        /**
+         * constructor 里加入 context 不然在组件里无法使用 context
+         */
         constructor(props, context) {
             super(props, context);
             this.state = {};
         }
+        /**
+         * 调用 getStore 来触发 render 从而触发 store.getState() 获取新的数据
+         */
         getStore = () => {
             this.setState({})
         }
+        /**
+         * componentDidMount 中绑定监听 this.getStore
+         * componentWillUnmount 中取消监听 this.getStore
+         */
         componentDidMount() {
             const { store } = this.context;
             this.unsubscribe = store.subscribe(this.getStore);
@@ -30,6 +40,9 @@ const connect = (mapState, mapDispatch) => (WrappedComponent) => {
             )
         }
     }
+    /**
+     * 定义 contextTypes 来拿到在 context 中的 store
+     */
     Connect.contextTypes = {
         store: PropTypes.object
     }
--- src/library/react-redux/react-redux_v15/provider.jsx
+++ src/library/react-redux/react-redux_v15/provider.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-
+/**
+ * Provider 组件接收两个 props: children 和 value
+ * children 所渲染的子元素
+ * value 为 uxunk 所创建的 store
+ */
 export default class Provider extends React.Component {
+    /**
+     * 将 this.props.value 当做 store 传递给 context
+     */
     getChildContext() {
         return {
             store: this.props.value
@@ -17,6 +24,10 @@ export default class Provider extends React.Component {
     }
 }
 
+/**
+ * 使用老版本的 context 必须使用 childContextTypes 定义
+ * 定义一个名称为 store 的 context
+ */
 Provider.childContextTypes = {
     store: PropTypes.object.isRequired
 }
